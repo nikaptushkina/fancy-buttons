15209a4784ba8119c0ff97fa39c25ff706975c2d
Nika Ptushkina <99292412+nika-ma@users.noreply.github.com>
2022-05-17 01:36:31 -0700

    Refactor state of light switch button

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,16 +1,14 @@
 import React from 'react'; //optional
-import { useState } from "react";
 
-  function LightSwitchButton(){
-    const [light, setLight] = useState('off');
+function LightSwitchButton(props){
 
-    const handleClick = () => setLight(light === "on" ? "off" : "on");
+  const {light, switchLight} = props
 
-    return (
-      <button onClick={handleClick} className="LightSwitchButton">
-        {light === "on" && <span><i>💡</i> I'm on!</span>}
-        {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
-      </button>
+  return (
+    <button onClick={switchLight} className="LightSwitchButton">
+      {light === "on" && <span><i>💡</i> I'm on!</span>}
+      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+    </button>
     );
   }
 
