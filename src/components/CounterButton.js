6f1e310f3fca6024ed0e84cbbabdd960d23eb5ff
Nika Ptushkina <99292412+nika-ma@users.noreply.github.com>
2022-05-17 01:20:23 -0700

    Modify state

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -3,9 +3,11 @@ import { useState } from "react";
 
   function CounterButton(){
     const [clickAmount, setClickAmount] = useState(0);
+    
+    const handleClick = () => setClickAmount(clickAmount + 1);
 
     return (
-      <button className="CounterButton">
+      <button onClick={handleClick} className="CounterButton">
         You clicked me {clickAmount} times
       </button>
     );
--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -3,13 +3,18 @@ import { useState } from "react";
 
   function TextRepeaterButton(){
     const [repetitions, setRepetitions] = useState(1);
+
     const textArray = [];
     for (let i = 0; i < repetitions; i++) {
-      textArray.push(<span key={i}>I like this text</span>);
+      textArray.push(<span key={i}> I like this text</span>);
+    }
+
+    const handleClick = () => {
+      setRepetitions(repetitions + 1);
     }
 
     return (
-      <button className="TextRepeaterButton">
+      <button onClick={handleClick} className="TextRepeaterButton">
         {textArray}
       </button>
     );
